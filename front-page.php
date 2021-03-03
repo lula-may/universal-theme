8643d41a2a37c85869e5cc1e62c23cad42488985
Yulia Elagina <julia-elagina@yandex.ru>
2021-03-04 01:08:14 +0300

    Настраивает вывод статей на главном экране

--- front-page.php
+++ front-page.php
@@ -3,44 +3,76 @@
   <div class="container">
     <div class="hero">
       <div class="hero-left">
-        <img src="<?php echo get_template_directory_uri() . '/assets/images/image.jpg' ?>" alt="" class="post-thumb">
-        <a href="#" class="author">
-          <img src="<?php echo get_template_directory_uri() . '/assets/images/avatar.png' ?>" alt="" class="author-avatar">
-          <div class="author-bio">
-            <span class="author-name">Имя Автора</span>
-            <span class="author-rank">Должность</span>
-          </div>
-        </a>
+        <?php
+          global $post;
 
-        <div class="post-text">
-          <a href="#" class="post-category category-name">Рубрики</a>
-          <h2 class="post-title">Название поста</h2>
-          <a href="#" class="post-more">Читать далее</a>
-        </div>
+          $myposts = get_posts([
+            'numberposts' => 1,
+          ]);
+
+          if( $myposts ){
+            foreach( $myposts as $post ){
+              setup_postdata( $post );
+              ?>
+          <!-- Вывода постов -->
+          <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="post-thumb">
+          <?php $author_id = get_the_author_meta('ID'); ?>
+          <a href="<?php echo get_author_posts_url($author_id) ?>" class="author">
+            <img src="<?php echo get_avatar_url($author_id) ?>" alt="" class="author-avatar">
+            <div class="author-bio">
+              <span class="author-name"><?php the_author(); ?></span>
+              <span class="author-rank">Должность</span>
+            </div>
+          </a>
+
+          <div class="post-text">
+            <?php the_category() ?>
+            <h2 class="post-title"><?php the_title(); ?></h2>
+            <a href="<?php echo get_the_permalink(); ?>" class="post-more">Читать далее</a>
+          </div>
+        <?php
+            }
+          } else {
+            ?>
+              <p>Постов нет</p>
+            <?php
+          }
+
+          wp_reset_postdata(); // Сбрасываем $post
+        ?>
       </div>
       <div class="hero-right">
         <h3 class="recommend">Рекомендуем</h3>
         <ul class="posts-list">
-          <li class="post">
-            <span class="post-category category-name">Категория</span>
-            <h4 class="post-title">Название поста в две строки</h4>
-          </li>
-          <li class="post">
-            <span class="post-category category-name">Категория</span>
-            <h4 class="post-title">Название поста в две строки</h4>
-          </li>
-          <li class="post">
-            <span class="post-category category-name">Категория</span>
-            <h4 class="post-title">Название поста в две строки</h4>
-          </li>
-          <li class="post">
-            <span class="post-category category-name">Категория</span>
-            <h4 class="post-title">Название поста в две строки</h4>
-          </li>
-          <li class="post">
-            <span class="post-category category-name">Категория</span>
-            <h4 class="post-title">Название поста в две строки</h4>
-          </li>
+          <?php
+          global $post;
+
+          $myposts = get_posts([
+            'numberposts' => 5,
+            'offset' => 1
+          ]);
+
+          if( $myposts ){
+            foreach( $myposts as $post ){
+              setup_postdata( $post );
+              ?>
+              <!-- Вывода постов -->
+              <li class="post">
+                <?php the_category(); ?>
+                <a class="post-permalink" href="<?php get_the_permalink(); ?>">
+                  <h4 class="post-title"><?php the_title(); ?></h4>
+                </a>
+              </li>
+              <?php
+              }
+            } else {
+              ?>
+                <p>Постов нет</p>
+              <?php
+            }
+
+          wp_reset_postdata(); // Сбрасываем $post
+        ?>
         </ul>
       </div>
     </div>
