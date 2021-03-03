0c81219abf9b02958329198f5e69884390acdeeb
Yulia Elagina <julia-elagina@yandex.ru>
2021-03-03 20:19:49 +0300

    Изменяет названия классов в блоке Hero

--- front-page.php
+++ front-page.php
@@ -2,10 +2,10 @@
 <main class="front-page-header">
   <div class="container">
     <div class="hero">
-      <div class="left">
+      <div class="hero-left">
         <img src="<?php echo get_template_directory_uri() . '/assets/images/image.jpg' ?>" alt="" class="post-thumb">
         <a href="#" class="author">
-          <img src="<?php echo get_template_directory_uri() . '/assets/images/avatar.png' ?>" alt="" class="avatar">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/avatar.png' ?>" alt="" class="author-avatar">
           <div class="author-bio">
             <span class="author-name">Имя Автора</span>
             <span class="author-rank">Должность</span>
@@ -13,32 +13,32 @@
         </a>
 
         <div class="post-text">
-          <a href="#" class="category-name">Рубрики</a>
+          <a href="#" class="post-category category-name">Рубрики</a>
           <h2 class="post-title">Название поста</h2>
-          <a href="#" class="more">Читать далее</a>
+          <a href="#" class="post-more">Читать далее</a>
         </div>
       </div>
-      <div class="right">
+      <div class="hero-right">
         <h3 class="recommend">Рекомендуем</h3>
         <ul class="posts-list">
           <li class="post">
-            <span class="category-name">Категория</span>
+            <span class="post-category category-name">Категория</span>
             <h4 class="post-title">Название поста в две строки</h4>
           </li>
           <li class="post">
-            <span class="category-name">Категория</span>
+            <span class="post-category category-name">Категория</span>
             <h4 class="post-title">Название поста в две строки</h4>
           </li>
           <li class="post">
-            <span class="category-name">Категория</span>
+            <span class="post-category category-name">Категория</span>
             <h4 class="post-title">Название поста в две строки</h4>
           </li>
           <li class="post">
-            <span class="category-name">Категория</span>
+            <span class="post-category category-name">Категория</span>
             <h4 class="post-title">Название поста в две строки</h4>
           </li>
           <li class="post">
-            <span class="category-name">Категория</span>
+            <span class="post-category category-name">Категория</span>
             <h4 class="post-title">Название поста в две строки</h4>
           </li>
         </ul>
