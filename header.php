13d778812ab64f4d8e72537e2c673b4810eccf97
Yulia Elagina <julia-elagina@yandex.ru>
2021-03-21 02:26:15 +0300

    изменяет цвет логотипа на внутренней странице, выводит блок автора поста, стилизует основной контент поста

--- header.php
+++ header.php
@@ -12,9 +12,9 @@
     <div class="header-wrapper">
       <?php
         if( has_custom_logo() ){
-          the_custom_logo();
+          echo '<div class="logo">' . get_custom_logo() . '<span class="logo-name">' .get_bloginfo('name') . '</span></div>';
         } else {
-          echo 'Universal';
+          echo '<span class="logo-name">' .get_bloginfo('name') . '</span>';
         }
       ?>
       <?php
